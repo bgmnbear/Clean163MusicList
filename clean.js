31c637e60374aeff71bc06b16a311a624242db0a
unknown <jasonwhister@gmail.com>
2018-05-20 15:12:41 +0800

    Add sleep() function

--- clean.js
+++ clean.js
@@ -3,25 +3,29 @@ const config = {
     listName: '我喜欢的音乐',
 }
 
-var es = function(name) {
+var es = function (name) {
     return document.getElementsByClassName(name)
 }
 
-var e = function(id) {
+var e = function (id) {
     return document.getElementById(id)
 }
 
+var sleep = function (d) {
+    for (var t = Date.now(); Date.now() - t <= d;) ;
+}
 
-var removeElement = function(name) {
+
+var removeElement = function (name) {
     name.parentNode.removeChild(name)
 }
 
-var collectSong = function(index) {
+var collectSong = function (index) {
     var e = es(' s-fc3')[index]
     e.getElementsByClassName('icn icn-fav')[0].click()
 }
 
-var chooseList = function(index) {
+var chooseList = function (index) {
     if (es('xtag ').length == 0) {
         var c1 = es('m-layer z-show m-layer-w5')[0]
         var c2 = es('m-layer z-show m-layer-w2')[0]
@@ -35,24 +39,24 @@ var chooseList = function(index) {
     }
 }
 
-var splitList = function(numOfSongs) {
+var splitList = function (numOfSongs) {
     var n = numOfSongs
     var index = 1
     for (var i = 2; i <= n; i++) {
         collectSong(i)
         chooseList(index)
-        if(i % config.num == 0) {
+        if (i % config.num == 0) {
             index += 1
         }
     }
 }
 
-var numOfSongs = function() {
+var numOfSongs = function () {
     var n = document.getElementById('flag_trackCount').textContent
     return Number(n)
 }
 
-var createList = function(name) {
+var createList = function (name) {
     es('u-btn u-btn-crt f-fr j-flag')[0].click()
     if (es('u-txt j-flag')[1] == undefined) {
         es('u-txt j-flag')[0].value = name
@@ -62,19 +66,20 @@ var createList = function(name) {
     es('u-btn2 u-btn2-2 u-btn2-w2 j-flag')[0].click()
 }
 
-var createLists = function(num) {
+var createLists = function (num) {
     for (var i = 1; i <= num; i++) {
         var name = String(i)
         createList(name)
+        sleep(5000)
     }
 }
 
-var _main = function() {
+var _main = function () {
     var n = numOfSongs()
     var numOfLists = Math.ceil(n / config.num)
 
     createLists(numOfLists)
-    splitList(n)
+    // splitList(n)
 }
 
 _main()
